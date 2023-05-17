a394d79702e7285e41bce707f79946e436d0fa43
brian24C <brianjosuecastro@hotmail.com>
2023-05-17 10:26:23 -0500

    refactoring-fetch games with React Query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 import apiClient from "../services/api-client";
-import { FetchResponse } from "./useData";
+import { FetchResponse } from "../services/api-client";
 
 //const useGenres = () => ({ data: genres, isLoading: false, error: null });
 
