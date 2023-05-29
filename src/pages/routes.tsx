2f84bfb57f5b1026570c4ac0ce034fca62a4f8ee
brian24C <brianjosuecastro@hotmail.com>
2023-05-29 11:35:43 -0500

    Handle Errors

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "./pages/Layout";
-import HomePage from "./pages/HomePage";
-import GameDetailPage from "./pages/GameDetailPage";
+import Layout from "./Layout";
+import HomePage from "./HomePage";
+import GameDetailPage from "./GameDetailPage";
+import ErrorPage from "../ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "", element: <HomePage /> },
       { path: "/games/:id", element: <GameDetailPage /> },
